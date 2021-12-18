9d787aec5f7103c03ce78d4f38730c2ceab78d56
Dimitrios Raptis <dimitriosr@canneslions.com>
2021-12-18 18:04:48 +0000

    Add emotion js on project, created index.style file as an example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Title from './index.style'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,9 +14,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Title>
           Welcome to <a href="https://nextjs.org">Next.js!!!</a>
-        </h1>
+        </Title>
 
         <p className={styles.description}>
           Get started by editing{' '}
